6a35a3224294dbd773975c623650b53111cf14d0
edKotinsky <edkotinsky@gmail.com>
2023-01-01 11:16:09 +0600

    list_boards, boardname_regexp

--- lua/arduino/cli.lua
+++ lua/arduino/cli.lua
@@ -8,9 +8,13 @@ local Cli = {
   --- Command to print configuration
   config_dump = { arduino_cli, 'config', 'dump' },
 
+  list_boards = { arduino_cli, 'board', 'listall' },
+
   --- Regex for finding the data path from 'arduino-cli config dump' output
   data_regexp_pattern =
-  '\\Mdata: \\zs\\[-\\/\\\\.[:alnum:]_~]\\+\\ze\\[[:space:]\\n]'
+  '\\Mdata: \\zs\\[-\\/\\\\.[:alnum:]_~]\\+\\ze\\[[:space:]\\n]',
+
+  boardname_regexp = '\\M \\zs\\w\\*:\\w\\*:\\w\\*'
 }
 
 return Cli
